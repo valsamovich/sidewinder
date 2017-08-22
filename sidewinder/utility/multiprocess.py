6b54577486df85a504743d86a78a489cfd3ffdcc
Valery <vsamov@users.noreply.github.com>
2017-08-22 12:01:56 -0400

    Create multiprocess.py

---
+++ sidewinder/utility/multiprocess.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python2.7
+# coding=utf-8
+
+from multiprocessing import Pool
+
+
+def f(x):
+    """ Worker thread logic.
+
+    :param x:   Input parameter, value, etc...
+    :return:    Result.
+    """
+    if x[1] in 'int':
+        return x[0] * x[0]
+    else:
+        return None
+
+if __name__ == '__main__':
+    # specify number of pools
+    p = Pool(processes=2)
+    print(p.map(f, [[2, 'string'], [3, 'int'], [4, 'int']]))
