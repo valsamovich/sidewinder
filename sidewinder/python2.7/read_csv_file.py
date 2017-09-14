27e8fe114cd7a4d4ab06de527acbaacbade9226e
Valery Samovich <valerysamovich@gmail.com>
2017-09-14 12:13:42 -0400

    Add exanple read file as UTF8

--- sidewinder/python2.7/read_csv_file.py
+++ sidewinder/python2.7/read_csv_file.py
@@ -1,23 +1,25 @@
 #!/usr/local/bin/python2.7
 
 import csv
+import codecs
 
 
 # open data fie using csv module
-def open_with_csv(filename):
+def open_with_csv(input_file):
     # create a list
     data = []
     # open a file with utf encoding
-    with open(filename, encoding='utf-8') as tsvin:
+    with codecs.open(input_file, 'r', 'utf-8') as utf8_file:
         # read line with csv reader
-        tie_reader = csv.reader(tsvin, delimiter='\t')
+        tie_reader = csv.reader(utf8_file, delimiter='\t')
         # for each line in data file upend to a list
         for line in tie_reader:
             data.append(line)
     # return a list
     return data
 
+
 # open file with csv
-data_from_csv = open_with_csv('data.csv')
+data_from_csv = open_with_csv('sample_data.csv')
 # print the header
 print data_from_csv[0]
