d3cff2a69c025e0a8fee489010d5eb9daffe270f
Valery <vsamov@users.noreply.github.com>
2020-06-15 00:31:56 -0400

    Update queue.py

--- sidewinder/data_structures/queue.py
+++ sidewinder/data_structures/queue.py
@@ -4,16 +4,16 @@
 # https://www.geeksforgeeks.org/queue-in-python/
 
 # Implementation using list
-# queue = []
-#
-# # Add elements by using append().
-# queue.append('a')
-# queue.append('b')
-# queue.append('c')
-# print('Initial queue:\n{}'.format(queue))
-#
-# # Remove elements by using pop().
-# print('Elements pop from the queue:')
-# print(queue.pop(0))
-# print(queue.pop(0))
-# print(queue.pop(0))
+queue = []
+
+# Add elements by using append().
+queue.append('a')
+queue.append('b')
+queue.append('c')
+print('Initial queue:\n{}'.format(queue))
+
+# Remove elements by using pop().
+print('Elements pop from the queue:')
+print(queue.pop(0))
+print(queue.pop(0))
+print(queue.pop(0))
